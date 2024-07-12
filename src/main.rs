b9be7740e51ba6453d5f018e6cc219044450887e
Santi <60857177+SSP-Programadores@users.noreply.github.com>
2024-07-13 00:41:28 +0200

    ventana no funcuina

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{thread, time::Duration};
 
 
 
-fn main() {
+/*fn main() {
     let mut reloj = ClockDisplay::new(); 
     let hilo = thread::spawn(move || {
         loop {
@@ -14,5 +14,28 @@ fn main() {
     });
     let _ = hilo.join();
 }
+*/
 
+use macroquad::prelude::*;
 
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let mut reloj = ClockDisplay::new(); 
+    
+    loop {
+        
+        
+        
+        clear_background(RED);
+
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        let hora = reloj.funciona();
+        draw_text(reloj.funciona(), 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
+    }
+    
+    
+}
